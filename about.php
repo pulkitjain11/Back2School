845d5500e16ab7849be9deb9ff953f5525e88649
Pulkit Jain <pulkitjainjk@gmail.com>
2022-06-11 16:39:06 +0530

    All Files

---
+++ about.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/abou.css">
+    <link rel="stylesheet" href="css/vitnav.css">
+    <link rel="stylesheet" href="css/foot.css">
+       <script src="https://kit.fontawesome.com/8f045748b8.js" crossorigin="anonymous"></script>
+       <link rel="icon" href="images/b2sLogofavi.png" type="image/gif">
+    <title>About Us</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <div class="container">
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<a class="navbar-brand" href="https://vit.ac.in"><img class="vit" src="images/vitlogo.png"></a>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav mr-auto">
+<li class="nav-item active">
+  <a class="nav-link" href="#" id="mathbee" style="color:white">Login<span class="sr-only">(current)</span></a>
+</li>
+<li class="nav-item active">
+  <a class="nav-link ali" href="about.php" style="color:white">AboutUs<span class="sr-only">(current)</span></a>
+</li>
+<li class="nav-item active">
+    <a class="navbar-brand" href="index.php"><img class="backs" src="images/B2S_Logo.png"></a>
+  </li>
+<li class="nav-item active">
+  <a class="nav-link ali" href="about.php" style="color:white">ContactUs<span class="sr-only">(current)</span></a>
+</li>
+<li class="nav-item active">
+  <a class="nav-link ali" href="http://info.vit.ac.in/gravitas2020/gravitas_login.asp" style="color:white">Register<span class="sr-only">(current)</span></a>
+</li>
+  <a class="navbar-brand" href="https://gravitas.vit.ac.in/index.html"><img class="gravi" src="images/graVITas20 Logo.png"></a>
+</ul>
+</div>
+</div>
+</nav>
+ <div class="yes">
+   <div class="container">
+         <h1 style="color: white; text-shadow: 5px 5px black; padding-top: 3rem;">ABOUT US</h1>
+         <div class="jumbotron">
+      <p class="lead">We are a team of students who aim to present an insight of college to school students through conducting events.
+        Through such events we bring in fun, learning and competition together. We encourage them to find a practical application to their
+        learnings and aim to boost their self-confidence. Back-2-School is event we conduct as an effort to fulfil our aim. We try to come
+        up with more interesting and fun ideas so that the students enjoy to the fullest in events like these.</p>
+         </div>
+    </div>
+ </div>
+
+ <footer id="contact"class="foot">
+   <div class="container">
+       <div class="row">
+
+           <div class="col-12 col-sm-6 footer-joinus">
+               <span class="footer-joinus-text">Have something to post or suggest?</span>
+               <a class="footer-joinus_button" href="mailto:b2svit@gmail.com">Mail us</a>
+           </div>
+           <div class="col-12 col-sm-6 align-self-center ">
+               <div class="text-center">
+                   <a class="btn btn-social-icon btn-google" href="https://www.instagram.com/back2schoolvit/"><i class="fa fa-instagram fa-lg"></i></a>
+                   <a class="btn btn-social-icon btn-google" href="tel:9445228496"><i class="fas fa-phone"></i></a>
+               </div>
+           </div>
+      </div>
+      <div class="row justify-content-center" style="margin-top: 10px;">
+           <div class="col-auto">
+               <p>© Copyright 2020 Back 2 School</p>
+           </div>
+      </div>
+   </div>
+ </footer>
+ <script>var contact=document.getElementById("contact");
+   function scroll_contact()
+   {contact.scrollIntoView({behavior: "smooth"});
+   }</script>
+        <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
